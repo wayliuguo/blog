82cb4c2b84a3e9fc688dc7e5bda2b3eb3fdab562
liuguowei <1085550655@qq.com>
2023-08-01 14:23:57 +0800

    feat: 添加react eslint校验

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         es2021: true,
         node: true
     },
-    extends: ['eslint:recommended', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'prettier'],
     overrides: [
         {
             env: {
@@ -16,11 +16,15 @@ module.exports = {
             }
         }
     ],
-    plugins: ['prettier'],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 'latest',
-        sourceType: 'module'
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
     },
+    plugins: ['@typescript-eslint', 'react', 'prettier'],
     rules: {
         indent: ['error', 4] // 用于指定代码缩进的方式，这里配置为使用四个空格进行缩进
     }
