404940ddeb14f104ac3381187ad2149d6edef3c0
liuguowei <1085550655@qq.com>
2023-09-20 23:23:21 +0800

    feat: styled-components

--- code/react/react-ts-demo/src/App.tsx
+++ code/react/react-ts-demo/src/App.tsx
@@ -2,23 +2,25 @@ import React, { useState } from 'react'
 // import logo from './logo.svg'
 import './App.css'
 import List from './List'
-import Count from './components/Count'
-import ClosureTrap from './components/ClosureTrap'
+/* import Count from './components/Count'
+import ClosureTrap from './components/ClosureTrap' */
+import StyledComponentsDemo from './components/StyledComponentsDemo'
 
 function App() {
-    const [showCountState, setCountState] = useState(true)
+    /* const [showCountState, setCountState] = useState(true)
     const destoryCount = () => {
         setCountState(false)
-    }
+    } */
     return (
         <>
             <List />
-            <hr />
+            {/* <hr />
             {showCountState && <Count />}
             <hr />
             <button onClick={destoryCount}>销毁count组件</button>
             <hr/>
-            <ClosureTrap/>
+            <ClosureTrap/> */}
+            <StyledComponentsDemo />
         </>
     )
 }
