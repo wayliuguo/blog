22104ccbafce2abb86cc4e9f4f809979850d1a95
liuguowei <1085550655@qq.com>
2023-10-07 14:01:22 +0800

    feat: useLoadQuestionListData 添加 page、pageSize

--- code/react/react-ts-demo/src/hooks/useLoadQuestionListData.ts
+++ code/react/react-ts-demo/src/hooks/useLoadQuestionListData.ts
@@ -1,7 +1,7 @@
 import { useSearchParams } from 'react-router-dom'
 import { useRequest } from 'ahooks'
 import { getQuestionListService } from '../services/question'
-import { LIST_SEARCH_PARAM_KEY } from '../constant'
+import { LIST_PAGE_PARAM_KEY, LIST_PAGE_SIZE, LIST_PAGE_SIZE_PARAM_KEY, LIST_SEARCH_PARAM_KEY } from '../constant'
 
 type OptionType = {
     isStar: boolean
@@ -14,7 +14,9 @@ const useLoadQuestionListData = (opt: Partial<OptionType> = {}) => {
     const { data, loading, error } = useRequest(
         async () => {
             const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
-            const data = await getQuestionListService({ keyword, isStar, isDeleted })
+            const page = parseInt(searchParams.get(LIST_PAGE_PARAM_KEY) || '') || 1
+            const pageSize = parseInt(searchParams.get(LIST_PAGE_SIZE_PARAM_KEY) || '') || LIST_PAGE_SIZE
+            const data = await getQuestionListService({ keyword, isStar, isDeleted, page, pageSize })
             return data
         },
         {
