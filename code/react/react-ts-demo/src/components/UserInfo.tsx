e1003293643f8263123cb96dd720ae9c0f975852
liuguowei <1085550655@qq.com>
2023-10-10 22:42:38 +0800

    feat: 登录token与退出token

--- code/react/react-ts-demo/src/components/UserInfo.tsx
+++ code/react/react-ts-demo/src/components/UserInfo.tsx
@@ -1,13 +1,42 @@
 import { FC } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { LOGIN_PATHNAME } from '../router'
+import { useRequest } from 'ahooks'
+import { getUserInfoService } from '../services/user'
+import { UserOutlined } from '@ant-design/icons'
+import { Button, message } from 'antd'
+import { removeToken } from '../utils/user-token'
 
 const UserInfo: FC = () => {
-    return (
+    const nav = useNavigate()
+
+    const { data } = useRequest(getUserInfoService)
+    const { username, nickname } = data || {}
+
+    // 退出登录
+    const logout = () => {
+        // 移除token
+        removeToken()
+        message.success('退出成功！')
+        // 跳转登录页
+        nav(LOGIN_PATHNAME)
+    }
+
+    const UserInfo = (
         <>
-            <Link to={LOGIN_PATHNAME}>登录</Link>
+            <span style={{ color: '#e8e8e8' }}>
+                <UserOutlined />
+                {nickname}
+            </span>
+            <Button onClick={logout} type="link">
+                退出
+            </Button>
         </>
     )
+
+    const Login = <Link to={LOGIN_PATHNAME}>登录</Link>
+
+    return <div>{username ? UserInfo : Login}</div>
 }
 
 export default UserInfo
