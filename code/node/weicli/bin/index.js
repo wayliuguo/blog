bf21aa9f811f16d8d2fe956e06abe313111ea351
liuguowei <1085550655@qq.com>
2024-02-06 22:51:07 +0800

    feat: yargs 框架入门

--- code/node/weicli/bin/index.js
+++ code/node/weicli/bin/index.js
@@ -1,33 +1,37 @@
 #!/usr/bin/env node
 
-const lib = require('weiclilib')
-const argv = require('process').argv
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 
-// 脚手架第一个参数
-const command = argv[2]
+const arg = hideBin(process.argv)
+const cli = yargs(arg)
 
-// 脚手架第一个参数后的参数
-const options = argv.slice(3)
-
-if (options.length) {
-    let [option, param] = options
-    option = option.replace('--', '')
-
-    if (command) {
-        if (lib[command]) {
-            lib[command]({ option, param })
-        } else {
-            console.log('请输入正确命令')
+yargs(arg)
+    // 用法介绍
+    .usage('Usage: weicli [command] <options>')
+    // 提示
+    .demandCommand(1, 'A command is required. Pass --help to see all avaliable commands and options.')
+    // 严格模式
+    .strict()
+    // 别名
+    .alias('h', 'help')
+    .alias('v', 'version')
+    // 设置文本宽度为terminal宽度
+    .wrap(cli.terminalWidth())
+    // 结尾文案
+    .epilogue('Your own footer description')
+    .options({
+        debug: {
+            type: 'boolean',
+            description: 'Boostrap debug mode',
+            alias: 'd'
         }
-    } else {
-        console.log('请输入命令')
-    }
-}
-
-// 实现全局参数解释 --version -V
-if (command.startsWith('--') || command.startsWith('-')) {
-    const globalOption = command.replace(/--|-/g, '')
-    if (globalOption === 'V' || globalOption === 'version') {
-        console.log('1.0.0')
-    }
-}
+    })
+    .options('registery', {
+        type: 'string',
+        describe: 'Define global registry',
+        alias: 'r'
+    })
+    // 分组
+    .group(['debug'], 'Dev Options:')
+    .group(['registry'], 'Extra Options').argv
