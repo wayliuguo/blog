2d003443cdd22156cbf3ebf527845e364617e86e
guoweiwliu <guoweiwliu@lexin.com>
2025-03-13 15:35:51 +0800

    feat: 文档整理

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
         nav: [
             {
                 text: '文档',
-                link: '/article/project/normalizeConfig',
+                link: '/article/lazyload',
                 activeMatch: '/article/'
             },
             {
@@ -187,141 +187,36 @@ module.exports = {
             ],
             '/article/': [
                 {
-                    text: '文档',
-                    items: [
-                        {
-                            text: '项目工程化',
-                            items: [
-                                {
-                                    text: 'vsCode配置&Prettier',
-                                    link: '/article/project/vsCodeConfigAndPrettier'
-                                },
-                                {
-                                    text: 'husky',
-                                    link: '/article/project/huskyUsage'
-                                },
-                                {
-                                    text: '规范化配置',
-                                    link: '/article/project/normalizeConfig'
-                                },
-                                {
-                                    text: '打包工具-webpack',
-                                    link: '/article/project/webpackConfig'
-                                },
-                                {
-                                    text: 'rollup&webpack',
-                                    link: '/article/project/rollup&vite'
-                                },
-                                {
-                                    text: '前端模块化规范',
-                                    link: '/article/project/moduleStandard'
-                                }
-                            ]
-                        },
-                        {
-                            text: '轮子原理',
-                            items: [
-                                {
-                                    text: '图片懒加载',
-                                    link: '/article/wheel/lazyload'
-                                },
-                                {
-                                    text: '虚拟滚动列表',
-                                    link: '/article/wheel/scrollList'
-                                }
-                            ]
-                        },
-                        {
-                            text: 'vue3 通用组件',
-                            items: [
-                                {
-                                    text: 'Monorepo',
-                                    link: '/article/vueComponent/monorepo'
-                                },
-                                {
-                                    text: 'ts 类型约束',
-                                    link: '/article/vueComponent/usets '
-                                },
-                                {
-                                    text: 'BEM 规范',
-                                    link: '/article/vueComponent/bem'
-                                }
-                            ]
-                        },
-                        {
-                            text: 'React',
-                            items: [
-                                {
-                                    text: 'react 基础',
-                                    link: '/article/react/reactBasic'
-                                },
-                                {
-                                    text: 'react hooks',
-                                    link: '/article/react/reactHooks'
-                                },
-                                {
-                                    text: 'react 使用ts指南',
-                                    link: '/article/react/reactUseTs'
-                                },
-                                {
-                                    text: 'react 使用css',
-                                    link: '/article/react/reactUseCss'
-                                },
-                                {
-                                    text: 'reactRouter',
-                                    link: '/article/react/reactRouter'
-                                },
-                                {
-                                    text: 'react 状态管理',
-                                    link: '/article/react/reactStateMan'
-                                }
-                            ]
-                        },
-                        {
-                            text: '常见需求处理',
-                            items: [
-                                {
-                                    text: '二进制',
-                                    link: '/article/requirement/binary'
-                                },
-                                {
-                                    text: '文件上传',
-                                    link: '/article/requirement/upload'
-                                }
-                            ]
-                        },
-                        {
-                            text: '沉淀',
-                            items: [
-                                {
-                                    text: 'EventBus 手写',
-                                    link: '/article/precipitation/eventbus'
-                                }
-                            ]
-                        },
-                        {
-                            text: 'vue3',
-                            items: [
-                                {
-                                    text: '手写vue3核心原理',
-                                    link: '/article/vue3/vue3stage'
-                                },
-                                {
-                                    text: 'vueUse 源码解析',
-                                    link: '/article/vue3/vueuse'
-                                }
-                            ]
-                        },
-                        {
-                            text: '经验总结',
-                            items: [
-                                {
-                                    text: '职业发展',
-                                    link: '/article/method/careeDevelopment'
-                                }
-                            ]
-                        }
-                    ]
+                    text: '图片懒加载',
+                    link: '/article/lazyload'
+                },
+                {
+                    text: '虚拟滚动列表',
+                    link: '/article/scrollList'
+                },
+                {
+                    text: 'Monorepo',
+                    link: '/article/monorepo'
+                },
+                {
+                    text: 'ts 类型约束',
+                    link: '/article/usets '
+                },
+                {
+                    text: 'BEM 规范',
+                    link: '/article/bem'
+                },
+                {
+                    text: '二进制',
+                    link: '/article/binary'
+                },
+                {
+                    text: '文件上传',
+                    link: '/article/upload'
+                },
+                {
+                    text: 'EventBus 手写',
+                    link: '/article/eventbus'
                 }
             ],
             '/interview': [
