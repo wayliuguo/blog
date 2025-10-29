ec1ba9c4ee6d2d22c19b1b4ddcc4066eacb7123a
liuguowei <1085550655@qq.com>
2025-10-29 23:07:07 +0800

    feat: 删除环境搭建文档

--- .vitepress/config/vue.js
+++ .vitepress/config/vue.js
@@ -1,11 +1,7 @@
 export default [
     {
-        text: 'vue3 原理',
+        text: '通过mini-vue 学习vue3原理',
         items: [
-            {
-                text: '环境搭建',
-                link: '/vue/vue3Principle/environment'
-            },
             {
                 text: 'vue3 源码架构',
                 link: '/vue/vue3Principle/architecture'
